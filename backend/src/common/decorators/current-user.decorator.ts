b245dd2d4a53fa4dc323a1ba452f097d4f393a79
Nigo <saouti.mossaab@etu.uae.ac.ma>
2026-07-10 10:09:04 +0100

    chore: remove unused common auth duplicates

--- backend/src/common/decorators/current-user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common'
-import { AuthUser } from '../../modules/auth/types/auth-user.type'
-
-export const CurrentUser = createParamDecorator(
-  (_data: unknown, context: ExecutionContext): AuthUser | null => {
-    const request = context.switchToHttp().getRequest<{ user?: AuthUser }>()
-    return request.user ?? null
-  },
-)
